7b49aa8ff2289c59e08b550c4e59f9f6a97c849f
ShubhamWadka12 <shubhamwadkar880@gmail.com>
2026-01-10 14:05:56 +0000

    Adding new file

---
+++ day_31_virtual_environment/main.py
@@ -0,0 +1,29 @@
+""" 
+🧩 What is virtual environment?
+
+A virtual environment is a separate place for one Python project.
+
+🤔 Why use it?
+
+So one project doesn’t spoil another project.
+
+🧠 Example
+
+> Project A uses one library
+
+> Project B uses another library
+
+> Virtual environment keeps them separate
+
+"""
+
+"""
+python -m venv venv
+
+venv\Scripts\activate   # Windows
+source venv/bin/activate  # Linux/Mac
+
+pip install something
+
+deactivate 
+"""
