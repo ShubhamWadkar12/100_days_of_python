1fcb2876bf40aa91f8bca32edcd1d0ec4f26c407
ShubhamWadka12 <shubhamwadkar880@gmail.com>
2026-01-10 14:05:10 +0000

    Adding new file

---
+++ day_29_short_hand_if_else/main.py
@@ -0,0 +1,19 @@
+""" age = 20
+if age >= 18:
+    result = "Adult"
+else:
+    result = "Minor"                                            #normal
+    
+    
+result = "Adult" if age >= 18 else "Minor"
+print(result)
+
+env = input("Enter the environment working ")
+envi = "Developement" if env == "Development" else f"{env}"
+print(envi)
+ """
+
+a = 30
+b = 40
+result = "A is greater than B " if a > b else "B is greater than A"
+print(result)
