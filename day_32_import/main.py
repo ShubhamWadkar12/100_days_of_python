51199dc332a887cba961a1a3e2af6edb49d7796b
ShubhamWadka12 <shubhamwadkar880@gmail.com>
2026-01-10 14:37:35 +0000

    Added new files

---
+++ day_32_import/main.py
@@ -0,0 +1,13 @@
+import math                                                     #Import full module
+print(math.sqrt(16))
+
+import math as m                                                #Short name (alias)
+print(m.sqrt(25))                       
+
+
+import random
+print(random.randint(1, 10))                                    #Python gives a random number
+
+
+from math import pi                                             #Import only what you need
+print(pi)
