bc76b9d0cbb5f1b8e94852da88dafb5741c8a686
ShubhamWadka12 <shubhamwadkar880@gmail.com>
2026-01-10 14:05:33 +0000

    Adding new file

---
+++ day_30_enumerate_function/main.py
@@ -0,0 +1,24 @@
+servers = ["web", "db", "cache"]
+
+i = 0
+for server in servers:
+    print(i, server)
+    i += 1
+
+fruits = ["Apples","Mango","Pinepapple"]
+
+i = 0
+for fruit in fruits:
+    print(i,fruit)
+    i+=1
+    
+    
+    
+servers = ["web", "db", "cache"]
+for i, server in enumerate(servers):
+    print(i, server)
+
+
+fruits = ["apples","Pineapple","Mango"]
+for i, fruit in enumerate(fruits):
+    print(i,fruit)
